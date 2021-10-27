683fd0bbdc4821c26d8b18a4b470a9f6485ac46e
BuildTools <unconfigured@null.spigotmc.org>
2021-10-27 00:34:00 -0400

    sghn

--- src/main/java/com/lronnobank/qpplugin/qpplugin.java
+++ src/main/java/com/lronnobank/qpplugin/qpplugin.java
@@ -1,6 +1,7 @@
 package com.lronnobank.qpplugin;
 
 import com.google.inject.Provides;
+import com.jogamp.newt.event.InputEvent;
 
 import javax.inject.Inject;
 
@@ -8,7 +9,6 @@ import lombok.extern.slf4j.Slf4j;
 import net.runelite.api.ChatMessageType;
 import net.runelite.api.Client;
 import net.runelite.api.GameState;
-import net.runelite.api.clan.ClanRank;
 import net.runelite.api.events.ChatMessage;
 import net.runelite.api.events.GameStateChanged;
 import net.runelite.client.chat.ChatMessageBuilder;
@@ -21,127 +21,139 @@ import net.runelite.client.plugins.PluginDescriptor;
 import net.runelite.client.util.Text;
 
 @Slf4j
-@PluginDescriptor(name = "qpplugin")
-public class qpPlugin extends Plugin {
+@PluginDescriptor(
+	name = "qpplugin"
+)
+public class qpPlugin extends Plugin
+{
 	@Inject
 	private Client client;
 	@Inject
-	private ExampleConfig config;
-	@Inject
+	private qpConfig config;
+    @Inject
 	private ChatMessageManager chatMessageManager;
 
 	@Override
-	protected void startUp() throws Exception {
-		/// log.info("Example started!");
+	protected void startUp() throws Exception
+	{
+	///	log.info("Example started!");
 	}
 
 	@Override
-	protected void shutDown() throws Exception {
-		// log.info("Example stopped!");
+	protected void shutDown() throws Exception
+	{
+		//log.info("Example stopped!");
 	}
 
 	@Subscribe
-	public void onGameStateChanged(GameStateChanged gameStateChanged) {
-		if (gameStateChanged.getGameState() == GameState.LOGGED_IN) {
-			client.addChatMessage(ChatMessageType.GAMEMESSAGE, "", "Standing by to W some q ps", null);
+	public void onGameStateChanged(GameStateChanged gameStateChanged)
+	{
+		if (gameStateChanged.getGameState() == GameState.LOGGED_IN)
+		{
+			client.addChatMessage(ChatMessageType.GAMEMESSAGE, "", "Example says " + config.greeting(), null);
 		}
 	}
 
 	@Provides
-	ExampleConfig provideConfig(ConfigManager configManager) {
-		return configManager.getConfig(ExampleConfig.class);
+	qpConfig provideConfig(ConfigManager configManager)
+	{
+		return configManager.getConfig(qpConfig.class);
 	}
+    private String[] chars = new String[] {
+        "!.:|iIl ",//3
+        "(),'`",//4
+        "{}\";[]rtT",//5 
+        "/\\-LcEfFjk1",//6
+        "+abBCdDeghHjKnopPqRsSuvVwxXyYzZ3457",//7
+        "^=$AGNOQU26890",//8
+        "_*mMW",//9
+        "",//10
+        "&%~",//11
+        "",//12
+        "#@"//13
+    };
 
-	private String[] chars = new String[] { 
-			"!.:|iIl ", // 3
-			"(),'`", // 4
-			"{}\";[]rtT", // 5
-			"/\\-LcEfFjk1", // 6
-			"+abBCdDeghHjKnopPqRsSuvVwxXyYzZ3457", // 7
-			"^=$AGNOQU26890", // 8
-			"_*mMW", // 9
-			"", // 10
-			"&%~", // 11
-			"", // 12
-			"#@"// 13
-	};
+    private int stringToPixel(String s) {
+        int res = 0;
+        for (int i = 0; i < s.length(); i++) {
+            res += charToPixel(s.charAt(i));
+        }
+        return res;
+    }
 
-	private int stringToPixel(String s) {
-		int res = 0;
-		for (int i = 0; i < s.length(); i++) {
-			res += charToPixel(s.charAt(i));
-		}
-		return res;
-	}
+    private int charToPixel(char c) {
+        for (int i = 0; i < chars.length; i++) {
+            if (chars[i].indexOf(c) != -1) {
+                return i + 3;
+            }
+        }
+        return 0;
+    }
 
-	private int charToPixel(char c) {
-		for (int i = 0; i < chars.length; i++) {
-			if (chars[i].indexOf(c) != -1) {
-				return i + 3;
-			}
-		}
-		return 0;
-	}
-
-	private String pixelsToString(int pixels) {
-		String result = "";
+    private String pixelsToString(int pixels) {
+        String result = "";
 		int unds = 0;
-		while (pixels >= 12) {
+        while (pixels >= 12) {
 			unds++;
-
-			result += (unds % 5 == 0 ? "_ " : "_");
-			pixels -= 9;
-		}
-		// result+=unds+"_";
-		if (pixels != 0) {
-			if (pixels == 10 || pixels == 12) {
-				pixels -= 4;
-				result += "(";
-			}
-			result += chars[pixels - 3].charAt(0);
-		}
-		return result;
-	}
-
-	@Subscribe
-	public void onChatMessage(ChatMessage event) {
-		int pixels = 0;
-		// final ClanRank rank = clanManager.getRank(event.getName());
-		// boolean cc = event.getType() == ChatMessageType.CLANCHAT;
-		// if (cc && rank != null && rank != ClanMemberRank.UNRANKED) {
-		// pixels += 14;
-		// }
-		String msg = event.getMessage();
+			
+            result += (unds%5==0?"_ ":"_");
+            pixels -= 9;
+        }
+	//	result+=unds+"_";
+        if (pixels != 0) {
+            if (pixels == 10 || pixels == 12) {
+                pixels -= 4;
+                result += "(";
+            }
+            result += chars[pixels - 3].charAt(0);
+        }
+        return result;
+    }
+@Subscribe
+public void onChatMessage(ChatMessage event){
+        int pixels = 0;
+    //    final ClanRank rank = clanManager.getRank(event.getName());
+    //     boolean cc = event.getType() == ChatMessageType.CLANCHAT;
+    //    if (cc && rank != null && rank != ClanMemberRank.UNRANKED) {
+    //        pixels += 14;
+    //    }
+        String msg = event..getMessage();
+		// Deb/ug(msg);
 		System.out.println(msg);
-		if (msg.indexOf("q p") == -1)
+		if(msg.indexOf("q p")==-1)
 			return;
-		String player = event.getName();
-		if (player.indexOf(">") != -1) {
-			pixels += 13;
-			player = player.substring(player.indexOf(">") + 1);
-		}
-		pixels += stringToPixel(Text.toJagexName(player));
-		pixels -= 13;
+        String player = event.getName();
+        String sender = event.getSender();
+        System.out.println(
+"p:"+player+" s:"+sender
+        );
+        
+        if (player.indexOf(">") != -1) {
+            pixels += 13;
+            player = player.substring(player.indexOf(">") + 1);
+        }
+        pixels += stringToPixel(Text.toJagexName(player));
+        pixels -= 13;
 		String nam = client.getLocalPlayer().getName();
 		System.out.println(nam);
-		pixels -= stringToPixel(nam);
-		// if (cc && clanManager.getRank("lronNoBank") != ClanMemberRank.UNRANKED) {
-		// pixels -= 14;
-		// }
-		String m = "";// + (cc ? "/" : "");
-		while (msg.indexOf("q p") != -1) {
-			pixels += stringToPixel(msg.substring(0, msg.indexOf("q p")));
-			pixels += 4;// line up w with q
-			m += pixelsToString(pixels) + "W";
-			pixels = 4;
-			msg = msg.substring(msg.indexOf("q p") + 3);
-		}
-
+        pixels -= stringToPixel(nam);
+     //   if (cc && clanManager.getRank("lronNoBank") != ClanMemberRank.UNRANKED) {
+      //      pixels -= 14;
+      //  }
+        String m = "";// + (cc ? "/" : "");
+        while (msg.indexOf("q p") != -1) {
+            pixels += stringToPixel(msg.substring(0, msg.indexOf("q p")));
+            pixels += 4;// line up w with q
+            m += pixelsToString(pixels) + "W";
+            pixels = 4;
+            msg = msg.substring(msg.indexOf("q p") + 3);
+        }
+        
 		System.out.println(m);
-		// client.addChatMessage(ChatMessageType.TRADE, "", m, null);
-		final String message = new ChatMessageBuilder().append(m).build();
-		chatMessageManager
-				// .queue(QueuedMessage.builder().type(ChatMessageType.TRADE).runeLiteFormattedMessage(m).build());
-				.queue(QueuedMessage.builder().type(ChatMessageType.TRADE).runeLiteFormattedMessage(message).build());
-	}
+			// client.addChatMessage(ChatMessageType.TRADE, "", m, null);
+         final String message = new ChatMessageBuilder().append(m).build();
+         chatMessageManager
+		 //.queue(QueuedMessage.builder().type(ChatMessageType.TRADE).runeLiteFormattedMessage(m).build());
+                .queue(QueuedMessage.builder().type(ChatMessageType.TRADE).runeLiteFormattedMessage(message).build());
+    }
 }
